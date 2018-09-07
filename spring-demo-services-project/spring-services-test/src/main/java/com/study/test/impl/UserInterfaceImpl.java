b76e80f322fccfe7c2e67154a382412669b35797
Loki <mic19940032@qq.com>
2018-09-07 18:14:23 +0800

    2018年9月7日18:13:37
    使用注解，JdbcDaoSupport实现数据库操作

--- spring-demo-services-project/spring-services-test/src/main/java/com/study/test/impl/UserInterfaceImpl.java
+++ spring-demo-services-project/spring-services-test/src/main/java/com/study/test/impl/UserInterfaceImpl.java
@@ -4,42 +4,58 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
 
 import com.study.test.bean.User;
 import com.study.test.interfaces.UserInterface;
 
-@Service
-public class UserInterfaceImpl implements UserInterface {
+@Repository
+public class UserInterfaceImpl extends JdbcDaoSupport implements UserInterface {
 
+//	@Autowired
+//	private JdbcTemplate jTemplate;
+
+	/*@Autowired
+    void UserVODaoImpl(DataSource dataSource) {
+        setDataSource(dataSource);
+    }*/
+	
 	@Autowired
-	private JdbcTemplate jTemplate;
+    private DataSource dataSource;
 
+    @PostConstruct
+    private void initialize() {
+        setDataSource(dataSource);
+    }
+	
 	@Override
 	public void save(User u) {
 		String sql = "insert into t_user(name) values(?)";
-		jTemplate.update(sql, u.getName());
+		super.getJdbcTemplate().update(sql, u.getName());
 	}
 
 	@Override
 	public void delete(Integer id) {
 		String sql = "delete from t_user where id=?";
-		jTemplate.update(sql, id);
+		super.getJdbcTemplate().update(sql, id);
 	}
 
 	@Override
 	public void update(User u) {
 		String sql = "update t_user set name=? where id=?";
-		jTemplate.update(sql, u.getName(), u.getId());
+		super.getJdbcTemplate().update(sql, u.getName(), u.getId());
 	}
 
 	@Override
 	public User getById(Integer id) {
 		String sql = "select * from t_user where id=?";
-		return jTemplate.queryForObject(sql, new RowMapper<User>() {
+		return super.getJdbcTemplate().queryForObject(sql, new RowMapper<User>() {
 
 			@Override
 			public User mapRow(ResultSet rs, int arg1) throws SQLException {
@@ -54,14 +70,14 @@ public class UserInterfaceImpl implements UserInterface {
 	@Override
 	public int getTotalCount() {
 		String sql = "select count(*) from t_user";
-		Integer count = jTemplate.queryForObject(sql, Integer.class);
+		Integer count = super.getJdbcTemplate().queryForObject(sql, Integer.class);
 		return count;
 	}
 
 	@Override
 	public List<User> getAll() {
 		String sql = "select * from t_user";
-		return jTemplate.query(sql, new RowMapper<User>() {
+		return super.getJdbcTemplate().query(sql, new RowMapper<User>() {
 
 			@Override
 			public User mapRow(ResultSet rs, int arg1) throws SQLException {
